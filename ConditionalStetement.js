d58f0a013a7580e71eafef40d3d981082be2ffdd
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-12 16:47:19 +0530

    Conditional Statement

---
+++ ConditionalStetement.js
@@ -0,0 +1,201 @@
+// *********** Conditional Statements *******************
+
+// if ************
+
+// let age = 31;
+// if(age >= 21) {   
+//     console.log("Eligiable For Marrige !! ");  
+// }
+
+
+// if Else ****************
+
+// let num = 12;
+
+// if(num % 2 === 0){
+//     console.log(num+" Even Number !!");
+    
+// }
+// else{
+//     console.log("Odd Number !!");
+    
+// }
+
+// let a = 10;
+// let b = 20;
+// let sum = a + b;
+
+// console.log("Additon of "+a+" And "+b+" Is : "+sum);
+
+// console.log("Additon of ",a," And ",b," Is : ",sum);
+
+// console.log(`Addition Of ${a} And ${b} Is : ${sum}`);
+
+
+// If Else Ladder ***************
+
+
+// let marks = 50;
+
+// if (marks > 90) {
+//     console.log("Grade : A");    
+// }
+// else if(marks > 80){
+//     console.log("Grade : B");
+// }
+// else if(marks > 70){
+//     console.log("Grade : C");
+// }
+// else if(marks > 60){
+//     console.log("Grade : D");
+// }
+// else if(marks > 50){
+//     console.log("Grade : E");
+// }
+// else{
+//     console.log("Fail !! ");    
+// }
+
+
+// let marks = 20;
+
+// let grade = (marks >= 90) ? "A+":
+//             (marks >= 80) ? "A" : 
+//             (marks >= 70) ? "B" :  
+//             (marks >= 60) ? "C" :
+//             (marks >= 50) ? "D" :
+//             "F";
+
+// console.log(`Your grade is: ${grade}`);
+
+
+
+// Nested If Else ***************
+
+// let num = 18;
+
+// if(num % 2 === 0)
+//     console.log("Even Number");
+    
+// else
+//     console.log("Odd Number");
+
+
+// let marks = 50;
+
+// if(marks >= 80){
+
+//     console.log("First Division !!");
+
+//     if(marks >= 90){
+//         console.log("Grade : A");  
+//     }
+//     else{
+//         console.log("Grade : B");
+        
+//     }
+    
+// }
+
+// else if(marks >= 60){
+//     console.log("Second Division!!");
+
+//     if(marks >= 70){
+//         console.log("Grade : C");  
+//     }
+//     else{
+//         console.log("Grade : D");
+        
+//     }  
+// }
+
+//  else{
+//     console.log("Fail !!😯😫");
+    
+//    } 
+    
+
+// Leap Year :- 
+/*
+2000 -> Century Year 
+            Leap Year
+
+ 2013 -> Normal Year
+ 
+            Not a Leap Year
+*/
+
+
+// let year = 1900;
+
+// if(year % 100 !== 0){
+//     console.log("Normal Year !!");
+
+//     if(year % 4 === 0){
+//         console.log(year,"Is Leap Year !!");  
+//     }
+//     else{
+//           console.log(year,"Is Not a Leap Year !!");
+//     }  
+// }
+
+// else{
+//     console.log("Century Year !!");
+    
+//      if(year % 400 === 0){
+//         console.log(year,"Is Leap Year !!");  
+//     }
+//     else{
+//           console.log(year,"Is Not a Leap Year !!");
+//     }
+// }
+
+
+// Switch ********************
+
+// let day = 3;
+
+// switch (day) {
+
+//     case 1:{
+//         console.log("Monday");  
+//         break;
+//             }
+
+//     case 2 : {
+//         console.log("Tuesday");
+//          break; 
+//     }  
+//     case 3:{
+//         console.log("Wednesday");
+//          break; 
+//     }   
+//     case 4:{
+//         console.log("Thrusday");
+//          break;
+//     }
+//     case 5 :{
+//         console.log("Friday");
+//          break; 
+//     }
+//     case 6 :{
+//         console.log("Saturday");
+//         break;  
+//     }
+//     case 7:{
+//         console.log("Sunday");
+//         break;  
+//     }
+//     default :{
+//         console.log("Invalid Input !!");
+        
+//     }
+        
+       
+// }
+
+
+
+
+
+
