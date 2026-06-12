2240966eeedf56a8f85b165fbf56b7d7430c1a8e
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-12 16:54:39 +0530

    practice

---
+++ Variables.js
@@ -0,0 +1,97 @@
+// Variables *********************************
+
+// var , let , const
+
+// var *************
+
+// var a; // decleration
+// // a = 20; // initilization
+
+// // a = 30; // re-initiliation
+
+// var a = 20; // re-decleration Allowed
+// console.log(a);
+
+// var b = 10 // decleration and initilization
+
+
+// Let 
+
+// let a ;
+// // a = 50;
+// // a = 60;
+
+// let a= 50; // re-declearation Not Allowed in let
+// console.log(a)
+
+//************************************
+
+// var password = 12345;
+
+// var password = 56789;
+
+// let password = 12345;
+
+// // let password = 56789; // Not Allowed
+
+// password = 56789;
+// console.log(password)
+
+
+// Const ********************
+
+// const a = 10;
+// // a = 20;   // Not-Allowed
+// console.log(a)
+
+// const password = "abc@235";
+
+// // password = "def@234567"; // Not Allowed
+
+// console.log(password)
+
+
+// Block Scope
+
+// var a = 10; // global variable
+
+// {
+//     var a = 20  // local 
+//     console.log(a);
+// }
+
+// console.log( a)
+
+
+
+// let a = 10;
+
+// {
+//     let a = 20
+//      console.log(a)
+// }
+//  console.log("outside",a)
+
+
+// const  a = 10
+
+// {
+//     const a = 20;
+//      console.log(a)
+// }
+//  console.log("outside",a)
+
+
+
+// functional Scope
+// var b = 10;
+// function checking () {
+// var a = 30;
+// console.log(b);
+// console.log(a);
+// }  
+// checking() 
+// // console.log(a);
+
+
+
