e2ac3b0ed452955a71f01e5f4ceeaff7ca6c5987
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-12 16:45:56 +0530

    array

---
+++ Array.js
@@ -0,0 +1,168 @@
+// Array *************************
+
+// let arr = [10, 20, 30, 40, 50, 40, "Neha", "Shyam"]; 
+
+// console.table(arr);
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.push(60);
+
+// arr.pop();
+
+// arr.unshift(5)
+
+// arr.shift();
+
+// console.log(arr);
+
+// console.log(arr.indexOf(40));
+
+// console.log(arr.lastIndexOf(40));
+
+// console.log(arr.includes(50));
+
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+
+// for(let i = 0; i < arr.length; i++){
+//     // console.log(arr[i]);
+
+//     if(arr[i] % 2 === 0){
+//         console.log(arr[i]);        
+//     }  
+// }
+
+
+// for(let i of arr){
+//     console.log(i);
+    
+// }
+
+// arr.forEach( (n)=>{
+//     console.log(n);   
+// } )
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// arr[2] = "Neha";
+
+// console.log(arr);
+
+
+// let newArray = arr.slice(2, 4)
+
+// console.log(newArray);
+// console.log(arr);
+
+
+
+// let newArray = arr.splice(2, 4);
+// console.log(newArray);
+
+// console.log(arr);
+
+
+// arr.splice(2, 2, "Neha", "Ram")
+// console.log(arr);
+
+
+
+
+// let arr = new Array(10, 20, 30, 40);
+
+// console.log(arr);
+
+// let str = "Neha"
+// // let arr = Array.from(str);
+// let arr = [...str]
+// console.log(arr);
+
+
+// let arr = Array.of(1 , 2, 3 , 4 ,5);
+// console.log(arr);
+
+
+// let arr = Array.of( 3 );
+// let arr1 = Array(3,4);  // newArray ki tarah agar hm likhte hai  to vo length ki property ko deta hai .
+// console.log(arr);
+// console.log(arr1);
+
+
+
+//  let arr = [1,2,3,4,5];
+//  let reverse = [];
+//  for(let i = arr.length-1; i>=0 ; i--){
+// reverse.push(arr[i])
+//  } 
+//  console.log(reverse);
+
+
+
+
+
+
+
+// let obj1 = {
+//     name: "Neha",
+//     age: 18,
+//     city: "Noida",
+// }
+
+// Object.values(obj1).forEach(value => {
+//     console.log(value);
+    
+// })
+
+
+ 
+
+// let obj = {
+//     name: "Neha",
+//     age: 18,
+//     city: "Noida",
+// }
+
+// for(let key in obj){
+//    console.log(key +" : " + obj[key]);   
+// }
+
+
+
+//  let obj1 ={
+//     name: "Satish",
+//     age: 19,
+    
+//   }
+
+//   let obj2 ={
+//        city: "Bahraich",
+//        Village: "Bhawanipur",
+//   }
+
+//   let obj3 ={
+//        education: "B.A",
+//        result: 555,
+//   }
+
+//   let obj4 ={
+//        hobbies: "Listing",
+//        Strength: "dutiful",
+//   }
+
+//   let newObject = {...obj1, ...obj2, ...obj3, ...obj4 };
+//   console.log(newObject);
+
+
+
+
+
