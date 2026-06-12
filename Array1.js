61d95d0298956bd44d64036532e9b94cb2717c68
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-12 16:46:34 +0530

    arraypractice

---
+++ Array1.js
@@ -0,0 +1,207 @@
+// Array *************************
+
+// let arr = [10, 20, 30, 40, 50, 40, "Neha", "Shyam"]; 
+
+// console.table(arr);
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.push(60);
+
+// arr.pop();
+
+// arr.unshift(5)
+
+// arr.shift();
+
+// console.log(arr);
+
+// console.log(arr.indexOf(40));
+
+// console.log(arr.lastIndexOf(40));
+
+// console.log(arr.includes(50));
+
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+
+// for(let i = 0; i < arr.length; i++){
+//     // console.log(arr[i]);
+
+//     if(arr[i] % 2 === 0){
+//         console.log(arr[i]);        
+//     }  
+// }
+
+
+// for(let i of arr){
+//     console.log(i);
+    
+// }
+
+// arr.forEach( (n)=>{
+//     console.log(n);   
+// } )
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// arr[2] = "Neha";
+
+// console.log(arr);
+
+
+// let newArray = arr.slice(2, 4)
+
+// console.log(newArray);
+// console.log(arr);
+
+
+
+// let newArray = arr.splice(2, 4);
+// console.log(newArray);
+
+// console.log(arr);
+
+
+// arr.splice(2, 2, "Neha", "Ram")
+// console.log(arr);
+
+
+
+
+// let arr = new Array(10, 20, 30, 40);
+
+// console.log(arr);
+
+
+
+// let arr = [10, 20, 30, 40, 50];
+
+// let reverse = arr.reverse();
+
+// console.log(reverse);
+
+
+
+// let name = ["Neha", "Vikash" , "Aditya", "Satish", "Rinku", "Shyam", "Daksha"];
+
+// let sortedName = name.sort();
+
+// console.log(sortedName);
+
+
+
+// let arr = [5, 6, 8, 2, 1];
+
+// let arr = [777, 65 , 11, 98, 34, 989]
+
+// let sortedNum = arr.sort( (a, b)=> a - b)
+
+// console.log(sortedNum);
+
+
+// let arr = [10, 20, 30, 40, 50];
+
+// // let str = arr.join(" : ");
+
+// let str = arr.toString();
+
+// console.log(typeof(str));
+
+
+// console.log(str);
+
+
+// let str = "Neha";
+
+// // let arr = Array.from(str);
+
+// // let arr = [...str]
+
+// console.log(arr);
+
+// let arr = Array.of(1, 2, 3, 4, 5);
+
+// console.log(arr);
+
+// let arr = Array.of(3);
+// let arr1 = Array(3);
+
+// console.log(arr);
+// console.log(arr1);
+
+
+// let arr1 = [1, 2, 3];
+
+// let arr2 = [4, 5, 6];
+
+// let arr3 = [7, 8, 9];
+
+// let arr4 = [10, 11, 12, 13, 14, 15];
+
+// arr1.push(arr2);
+
+// console.log(arr1);
+
+// let newArray = arr1.concat(arr2);
+
+// let arr = [...arr1, ...arr2, ...arr3, ...arr4]
+
+// console.log(arr);
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// // console.log( arr.some( n =>  n > 6) );
+
+// console.log( arr.every(n => n >= 1));
+
+
+
+// map()
+// filter()
+// reduce()
+
+// forEach()
+
+
+let arr = [1, 2 , [10, 20, [100, 200, [1000, 2000, [10000, 20000], 3000, 4000, ], 300, 400 ], 30, 40 ], 3, 4 ];
+
+
+console.log(arr.flat(Infinity));
+
+// let str = "Neha";
+// console.log(Array.isArray(str));
+
+// let arr = [10, 20, 30];
+
+// console.log(Array.isArray(arr));
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// [5, 4, 3, 2, 1] 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
