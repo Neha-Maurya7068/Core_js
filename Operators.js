4785f704d52b2f366d7a8b0c5319e1e43547e6c2
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-12 16:52:48 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,183 @@
+// *********** Operators ******************
+
+// ******* Arith... ***************
+
+// let a = 10;
+// let b = 6;
+
+// // console.log(a + b);
+// // console.log(a - b);
+
+// // console.log(a * b);
+
+// console.log(a / b);
+// console.log(Math.floor(a / b));
+
+// console.log(a % b);
+
+// console.log(a ** b);
+
+// console.log(5 ** 2);
+
+// console.log(2 ** 5);
+
+
+// Unary *************
+
+// let a = 10;
+
+// console.log(a++);
+// console.log(a);
+
+// console.log(++a);
+// console.log(a);
+
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+
+// console.log(a++ + b++);
+// console.log(a + b);
+
+// console.log(++a + ++b);
+// console.log(a+b);
+
+// a = 10;
+// a++;
+// console.log(a);
+
+
+// let a = 10;
+// a = a++;
+// a = ++a;
+// console.log(a);
+// console.log(a);
+
+
+
+// console.log(a);
+// console.log(b);
+
+//********* Question ******* */
+// let a = 20;
+// a++;
+
+// let b = a++;
+
+// let c = ++b;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// let a = 13;
+
+// a = a++
+// a = a++
+// a = a++
+// a = a++
+
+// a = a--;
+// a = a--;
+
+// a = a++;
+
+// console.log(a);
+
+// Assignment *******************
+
+// let a = 20;
+// // a = a + 30;
+
+// // a += 30;
+// a *= 2;
+// console.log(a);
+
+// Relational / Comp..... *************
+
+// console.log(10 > 20);
+// console.log(10 < 20);
+
+// console.log(10 != 10);
+
+// console.log(20 > 20);
+
+// console.log(20 < 20);
+
+
+// console.log(20 <= 20);
+
+
+// console.log(2 == "2");
+
+// console.log(2 === "2");
+// !==
+
+// Logical ****************
+
+// AND &&
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// OR ||
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+ 
+// NOT !
+
+// console.log(!true);
+// console.log(!false);
+
+// Bitwise Operator *****************
+
+// console.log(5 & 1);
+// console.log(7 & 1);
+
+
+// console.log(5 | 1);
+// console.log(7 | 1);
+
+// console.log(5 | 2);
+
+// console.log(5 ^ 1);
+
+// console.log(5 ^ 2 );
+
+// console.log(5 >> 1);
+// console.log(5 >> 2);
+
+
+// console.log(5 << 1);
+
+// console.log(5 << 2);
+
+
+// **************Ternary Operator*********************
+// let year = 3200;
+// let result=(year%100 !==0 && year%4 ===0) || (year%400 ===0)?
+//  "Leap Year" : "Not a Leap year";
+// console.log(result);
+
+
+// let hour = 13;
+// let message=(hour < 12)? "Good Morning!" : "Good Afternoon";
+// console.log(message);
+
+
+
+
+let year1 = 2000;
+let result1 = (year1%100 !==0 && year1%4 ===0) || (year1%4000 ===0)?
+"Leap Year" : "Not a Leap year";
+console.log(result1);
+
+
