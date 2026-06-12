f3fa76e200d26e283689e3be2ed2eefdf9724ad3
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-12 16:54:10 +0530

    practice with variable

---
+++ Variable2.js
@@ -0,0 +1,64 @@
+// *****************************************
+
+
+
+// console.log(x);
+
+
+
+
+// console.log(x);
+
+// console.log("Hello Js 1");
+// console.log("Hello Js 2");
+// console.log("Hello Js 3");
+// console.log("Hello Js 4");
+// console.log("Hello Js 5");
+// console.log("Hello Js 6");
+// console.log("Hello Js 7");
+// console.log("Hello Js 8");
+// console.log("Hello Js 9");
+// console.log("Hello Js 10");
+
+
+// let x;
+
+
+// greet();
+
+// function greet() {
+//     console.log("Good Morning !! ");
+    
+// }
+
+
+
+// ()=>{}
+
+//    data("Neha")
+
+//     var data = (n) =>{
+//         console.log(n);
+        
+//     }
+
+    // data("Neha")
+
+// ()()
+// (function() {
+//     console.log("Hello Js");
+    
+// })()
+
+
+// console.log(10 == "10");
+
+// console.log(10 === "10");
+
+
+// console.log(2 + "55");
+
+// console.log("55" - 3);
+
+
+// 
