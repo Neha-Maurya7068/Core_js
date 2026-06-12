5a1c42b570c7a071bf3c1327070178276fd6862c
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-12 16:47:46 +0530

    Datatypes

---
+++ DataTypes.js
@@ -0,0 +1,111 @@
+// **************************** DataType ***********************
+
+// let a = 10;
+
+//  b = a;
+
+// b = b + 10;
+
+//  console.log(b);
+
+// console.log(a);
+
+// Single Line
+
+/*
+Multi Line
+*/
+
+/**
+ * 
+ * Discriptive Comment
+ * 
+ * 
+ * 
+ */
+
+// let obj = {
+//     name : "Neha",
+//     age : 20,
+
+// }
+// // console.log(obj);
+
+// let obj2 = obj;
+
+// // console.log(obj2);
+// obj2.city = "Delhi"
+
+// console.log(obj2);
+
+// console.log(obj);
+
+
+
+// let a ;
+// console.log(a);
+
+// Null
+
+// console.log(Boolean(null == 0));
+// console.log(Boolean(null > 0));
+// console.log(Boolean(null < 0));
+
+// console.log(Boolean(null >= 0));
+// console.log(Boolean(null <= 0));
+
+// Umdefined
+
+// console.log(Boolean(undefined == 0));
+// console.log(Boolean(undefined > 0));
+// console.log(Boolean(undefined < 0));
+
+// console.log(Boolean(undefined >= 0));
+// console.log(Boolean(undefined <= 0));
+
+// BigInt
+
+// let a = 12n;
+// console.log(typeof(a));
+
+
+// number 
+
+// console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+
+
+// Symbol
+
+// id = 101;
+// id1 = Symbol(id);
+// id2 = Symbol(id)
+
+// // console.log(id2);
+// console.log(id1 == id2);
+
+
+// string
+
+// let s = "Neha";
+// // console.log(s);
+// console.log(typeof(s));
+
+// Non-Primitive *************
+
+
+// console.log(typeof(function(){}));
+
+// console.log(typeof( {} ));
+
+// console.log(typeof([]));
+
+
+// console.log(null == undefined);
+
+// console.log(typeof(null));
+ 
+
+
+
+
+
